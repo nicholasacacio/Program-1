d3f50053b6287d07a150c2c915d65b5e6d7ab76c
nicholasacacio <acacionicholas.cdm@gmail.com>
2026-01-19 06:06:53 +0000

    Game V1.0

---
+++ game.py
@@ -0,0 +1,11 @@
+# Aplikasi game secret number
+
+secret_number = 777
+guess_number = int(input("Masukkan Tebak Angka:"))
+
+while guess_number != secret_number:
+    print("Tebakan salah, silakan coba lagi")
+    guess_number = int(input("Masukkan Tebak Angka:"))
+
+print("Selamat.., tebakan Anda benar")
+print("Kode ini saya buat di codespace")
